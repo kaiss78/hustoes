4a8c87153653ed76fdee69170f8f99ffe7e71c56
pl8787 <pl8787@4cc9049f-d595-6ec1-9b74-94d1f650cfd7>
2010-11-15 13:17:39 +0000

    3个pD类

---
+++ OES/Model/OfficeExcel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OES.Model
+{
+    class OfficeExcel:Problem
+    {
+        public string rawPath;
+        public string ansPath;
+        public string stuAnsPath;
+        
+        public OfficeExcel()
+        {
+            type = "Excel操作题";
+        }
+        public OfficeExcel(string rawPath, string ansPath, string stuAnsPath)
+        {
+            this.ansPath = ansPath;
+            this.rawPath = rawPath;
+            this.stuAnsPath = stuAnsPath;
+            type = "Excel操作题";
+        }
+    }
+}
---
+++ OES/Model/OfficePowerPoint.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OES.Model
+{
+    class OfficePowerPoint:Problem
+    {
+        public string rawPath;
+        public string ansPath;
+        public string stuAnsPath;
+        
+        public OfficePowerPoint()
+        {
+            type = "PowerPoint操作题";
+        }
+        public OfficePowerPoint(string rawPath, string ansPath, string stuAnsPath)
+        {
+            this.ansPath = ansPath;
+            this.rawPath = rawPath;
+            this.stuAnsPath = stuAnsPath;
+            type = "PowerPoint操作题";
+        }
+    }
+}
