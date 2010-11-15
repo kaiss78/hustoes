376157ae31436220b374341c2a60e87aa0b1600f
pl8787 <pl8787@4cc9049f-d595-6ec1-9b74-94d1f650cfd7>
2010-11-15 13:17:39 +0000

    3个pD类

---
+++ trunk/OES/Model/OfficeWord.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OES.Model
+{
+    class OfficeWord:Problem
+    {
+        public string rawPath;
+        public string ansPath;
+        public string stuAnsPath;
+        
+        public OfficeWord()
+        {
+            type = "Word操作题";
+        }
+        public OfficeWord(string rawPath, string ansPath, string stuAnsPath)
+        {
+            this.ansPath = ansPath;
+            this.rawPath = rawPath;
+            this.stuAnsPath = stuAnsPath;
+            type = "Word操作题";
+        }
+    }
+}
