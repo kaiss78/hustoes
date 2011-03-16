33a6cba7e585296c93a787a61b0b2c28b232e143
pl8787 <pl8787@4cc9049f-d595-6ec1-9b74-94d1f650cfd7>
2011-03-16 12:12:38 +0000

    To .Net Framework2.0

--- OES/UControl/CustomExcel.cs
+++ OES/UControl/CustomExcel.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Drawing;
 using System.Data;
-using System.Linq;
+ 
 using System.Text;
 using System.Windows.Forms;
 using System.IO;
@@ -98,7 +98,7 @@ namespace OES.UControl
 //using System.ComponentModel;
 //using System.Drawing;
 //using System.Data;
-//using System.Linq;
+// 
 //using System.Text;
 //using System.Windows.Forms;
 //using System.Xml;
