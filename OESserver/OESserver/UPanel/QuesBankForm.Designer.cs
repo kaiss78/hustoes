673323e48b7037b5b20654b10874bac123a439b2
qinzhaoxi <qinzhaoxi@gmail.com>
2011-12-06 11:08:33 +0000

    点击

--- OESserver/OESserver/UPanel/QuesBankForm.Designer.cs
+++ OESserver/OESserver/UPanel/QuesBankForm.Designer.cs
@@ -65,6 +65,7 @@
             this.button1.TabIndex = 2;
             this.button1.Text = "添加试题";
             this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
@@ -205,7 +206,6 @@
             this.Controls.Add(this.button1);
             this.Controls.Add(this.dataGridView1);
             this.Name = "QuesBankForm";
-            this.Size = new System.Drawing.Size(742, 666);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
--- OESserver/OESserver/UPanel/QuesBankForm.cs
+++ OESserver/OESserver/UPanel/QuesBankForm.cs
@@ -12,12 +12,23 @@ namespace OES.UPanel
     public partial class QuesBankForm : UserPanel
     {
         private DataTable questionTable;
+
+        public void InitDT()
+        {
+            questionTable = new DataTable("questionlist");
+            questionTable.Columns.Add("选择",typeof(bool));
+            questionTable.Columns.Add("题名");
+            questionTable.Columns.Add("题型");
+            questionTable.Columns.Add("章节");
+            questionTable.Columns.Add("难度");
+        }
+
         public QuesBankForm()
         {
             InitializeComponent();
 
-
         }
+
         public override void ReLoad()
         {
             this.Visible = true;
@@ -26,6 +37,11 @@ namespace OES.UPanel
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            PanelControl.ChangPanel(8);
         }
     }
 }
