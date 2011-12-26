889ae10d2d0745180499447050fd9e868051f912
pl8787 <pl8787@4cc9049f-d595-6ec1-9b74-94d1f650cfd7>
2011-12-26 02:17:40 +0000

    Invoke->BeginInvoke

--- trunk/OESMonitor/OESMonitor/CommandLine.cs
+++ trunk/OESMonitor/OESMonitor/CommandLine.cs
@@ -13,7 +13,6 @@ namespace OESMonitor
     public partial class CommandLine : Form
     {
         public string logFileName = "";
-        private string _msg;
 
         private delegate void ShowMessage();
         public CommandLine(string logFileName)
@@ -24,22 +23,17 @@ namespace OESMonitor
         }
         public void showMessage(string message)
         {
-            _msg = message;
-            listView1.Invoke(new ShowMessage(UpdateUI));
-            //ServerMessage_listBox.Items.Add(message);
-        }
-
-        private void UpdateUI()
-        {
-            if (!string.IsNullOrEmpty(logFileName))
-            {
-                using (StreamWriter sw = new StreamWriter(logFileName, true, Encoding.Default))
+            listView1.BeginInvoke(new MethodInvoker(() => {
+                if (!string.IsNullOrEmpty(logFileName))
                 {
-                    sw.WriteLine(_msg);
+                    using (StreamWriter sw = new StreamWriter(logFileName, true, Encoding.Default))
+                    {
+                        sw.WriteLine(message);
+                    }
                 }
-            }
-            listView1.Items.Add(_msg);
-            listView1.SelectedIndex = listView1.Items.Count - 1;
+                listView1.Items.Add(message);
+                listView1.SelectedIndex = listView1.Items.Count - 1;
+            }));
         }
     }
 }
